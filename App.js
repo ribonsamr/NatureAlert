696f988c3e58494c6d40ba2d6aa0af678963ae54
Amr <amressam01@hotmail.com>
2018-10-14 14:48:51 +0200

    Try to touch list item

--- App.js
+++ App.js
@@ -11,6 +11,61 @@ import {createStackNavigator} from 'react-navigation';
 
 type Props = {};
 
+class MyListItem extends React.PureComponent {
+  _onPress = () => {
+    this.props.navigation.navigate('Details', {title: this.props.title})};
+  }
+ // sasd
+  render() {
+    return (
+      <TouchableOpacity onPress={this._onPress}>
+        <View>
+          <Text style={styles.item}>
+            {this.props.title}
+          </Text>
+        </View>
+      </TouchableOpacity>
+    );
+  }
+}
+
+class MultiSelectList extends React.PureComponent {
+  // state = {selected: (new Map(): Map<string, boolean>)};
+
+  // _keyExtractor = (item, index) => item.id;
+
+  // _onPressItem = (id: string) => {
+  //   // updater functions are preferred for transactional updates
+  //   this.setState((state) => {
+  //     // copy the map rather than modifying state.
+  //     const selected = new Map(state.selected);
+  //     selected.set(id, !selected.get(id)); // toggle
+  //     return {selected};
+  //   });
+  //   this.props.navigation.navigate('Details', {title: 'Earthquake'})}
+  // };
+
+  _renderItem = ({item}) => (
+    <MyListItem
+      // id={item.id}
+      // onPressItem={this._onPressItem}
+      // selected={!!this.state.selected.get(item.id)}
+      title={item.title}
+    />
+  );
+
+  render() {
+    return (
+      <FlatList
+        data={this.props.data}
+        // extraData={this.state}
+        // keyExtractor={this._keyExtractor}
+        renderItem={this._renderItem}
+      />
+    );
+  }
+}
+
 class HomeScreen extends React.Component {
   static navigationOptions = {
     title: 'Home',
@@ -23,13 +78,13 @@ class HomeScreen extends React.Component {
       <View>
         <Text style={styles.welcome}>Disasters</Text>
         <View>
-          <FlatList style={styles.list}
-            data={[{key: 'Earthquake'}, {key: 'Lava'}]}
-            renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
+          <MultiSelectList style={styles.list}
+            data={[{id:0, title: 'Earthquake'}, {id: 1, title: 'Lava'}]}
+            // renderItem={({item}) => <Text style={styles.item}>{item.key}</Text>}
           />
           <Button
             title="Go to Details"
-            onPress={() => this.props.navigation.navigate('Details')}
+            onPress={() => this.props.navigation.navigate('Details', {title: 'Earthquake'})}
           />
         </View>
       </View>
@@ -38,6 +93,12 @@ class HomeScreen extends React.Component {
 }
 
 class DetailsScreen extends React.Component {
+  static navigationOptions = ({ navigation }) => {
+    return {
+      title: navigation.getParam('title'),
+    };
+  };
+
   render() {
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
