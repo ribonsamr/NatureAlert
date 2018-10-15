cbbc49547b223758dbe08ff06e1f00b2737138f7
Amr <amressam01@hotmail.com>
2018-10-16 00:17:05 +0200

    Revert back to basic FlatList

--- App.js
+++ App.js
@@ -11,61 +11,6 @@ import {createStackNavigator} from 'react-navigation';
 
 type Props = {};
 
-class MyListItem extends React.PureComponent {
-  _onPress = () => {
-    this.props.navigation.navigate('Details', {title: this.props.title})};
-  }
- // sasd
-  render() {
-    return (
-      <TouchableOpacity onPress={this._onPress}>
-        <View>
-          <Text style={styles.item}>
-            {this.props.title}
-          </Text>
-        </View>
-      </TouchableOpacity>
-    );
-  }
-}
-
-class MultiSelectList extends React.PureComponent {
-  // state = {selected: (new Map(): Map<string, boolean>)};
-
-  // _keyExtractor = (item, index) => item.id;
-
-  // _onPressItem = (id: string) => {
-  //   // updater functions are preferred for transactional updates
-  //   this.setState((state) => {
-  //     // copy the map rather than modifying state.
-  //     const selected = new Map(state.selected);
-  //     selected.set(id, !selected.get(id)); // toggle
-  //     return {selected};
-  //   });
-  //   this.props.navigation.navigate('Details', {title: 'Earthquake'})}
-  // };
-
-  _renderItem = ({item}) => (
-    <MyListItem
-      // id={item.id}
-      // onPressItem={this._onPressItem}
-      // selected={!!this.state.selected.get(item.id)}
-      title={item.title}
-    />
-  );
-
-  render() {
-    return (
-      <FlatList
-        data={this.props.data}
-        // extraData={this.state}
-        // keyExtractor={this._keyExtractor}
-        renderItem={this._renderItem}
-      />
-    );
-  }
-}
-
 class HomeScreen extends React.Component {
   static navigationOptions = {
     title: 'Home',
