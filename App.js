d44f0e2823d53e199184220dbe43dce34a4aba4d
Amr <amressam01@hotmail.com>
2018-10-17 23:44:34 +0200

    Handle renderDisasters the comp. rendering

--- App.js
+++ App.js
@@ -53,15 +53,15 @@ class DetailsScreen extends React.Component {
   renderDisasters(param) {
     switch (param) {
       case "Earthquake":
-        return 'quake baby';
+        return <Text>quake baby</Text>;
       case "Volcano":
-        return 'Volcano baby';
+        return <Text>Volcano baby</Text>;
       case "Storm":
-        return 'storm baby';
+        return <Text>storm baby</Text>;
       case "Typhoon":
-        return 'typhoon baby';
+        return <Text>typhoon baby</Text>;
       default:
-        return 'param';
+        return <Text>{param}</Text>;
     }
   }
 
@@ -71,8 +71,7 @@ class DetailsScreen extends React.Component {
 
     return (
       <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-
-        <Text>{this.renderDisasters(title)}</Text>
+        {this.renderDisasters(title)}
       </View>
     );
   }
