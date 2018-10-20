24521a784cd889223d1e26a3b36296049ee23103
Amr <amressam01@hotmail.com>
2018-10-20 03:37:06 +0200

    Stable

--- App.js
+++ App.js
@@ -105,12 +105,9 @@ class EmergencyScreen extends React.Component {
       (error) => {this.setState({ error: error.message });
         Alert.alert(error.message);
       },
-<<<<<<< HEAD
       (error) => {this.setState({ error: error.message });
         // Alert.alert(error.message);
       },
-=======
->>>>>>> e52031275ba607625cc330376fd612930a0a5be1
       { enableHighAccuracy: true, timeout: 20000, maximumAge: 1000 },
     );
   }
